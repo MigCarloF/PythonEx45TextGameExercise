2ebaf33fc0693ca0ea7012a49685cd9dbcd8d29e
Miguel Carlo Fernandez <carlo@symph.co>
2019-07-03 13:40:23 +0800

    Cleanup. Added game_start() and tests()

--- Text Adventure/Game/Main.py
+++ Text Adventure/Game/Main.py
@@ -2,9 +2,19 @@ import SceneMap, Engine
 
 
 def main():
+    tests()
+
+
+def game_start():
     map = SceneMap.SceneMap("house")
     game = Engine.Engine(map)
     game.run()
+
+
+def tests():
+    x = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+
+    print x.index(1)
 
 
 if __name__ == "__main__":
